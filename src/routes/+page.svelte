cd4a064b9dcf1e9f81ab857ca3326e6365412117
Nils Leijdekker <nilsleijdekker@gmail.com>
2022-11-06 22:44:13 +0100

    Tryed to get ID api fetch working, didn't work.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,39 @@
 <script lang="ts">
-	let name: any;
+    import { onMount } from 'svelte';
+  import { debug } from 'svelte/internal';
+	let username: any;
+    let playerData: any;
+    let playerId: any;
+    let time: number;
+    let ashconEndpoint: string = "https://api.ashcon.app/mojang/v2/user/";
 
-    function updateName(e:any){
-        name = e.target.value;
+	onMount(() => {
+		time = Date.now();
+        console.log(time);
+	});
+    function updateUsername(e:any){
+        username = e.target.value;
+        getPlayerData();
+        
+    }
+
+    async function getPlayerData(){
+        if(fetch("https://api.ashcon.app/mojang/v2/user/"+username) != null){
+            const apiOutput = await fetch("https://api.ashcon.app/mojang/v2/user/"+username);
+            playerData = apiOutput.json();
+        }
+    }
+
+    function setID(){
+        playerId = playerData.value.uuid;
+        console.log(playerData.value)
     }
 </script>
 
-<input type="text" on:input={updateName}/>
-<p>Hello {name}!</p>
+<div class="grid grid-cols-1 grid-rows-3">
+    <input type="text" on:input={updateUsername} on:change={getPlayerData}/>
+
+    <button on:click={setID} class=""></button>
+
+    <h1>{playerId}</h1>
+</div>
