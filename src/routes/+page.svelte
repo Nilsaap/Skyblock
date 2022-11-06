4a3791b33bd33ede88ccc8ad0fe7379ed33006c3
Nils Leijdekker <nilsleijdekker@gmail.com>
2022-11-06 10:52:01 +0100

    worked on routing and started home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	let name = '';
+</script>
+
+<input bind:value={name} placeholder="enter your name">
+<p>Hello {name}!</p>
---
+++ src/routes/player-data/[id]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { debug } from 'svelte/internal';
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+  console.log(data);
+</script>
+
+<h1>{data.id}</h1>
